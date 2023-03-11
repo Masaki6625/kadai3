d33a8fd35a756d913b7f49a516ed2b635881ebe6
EC2 Default User <ec2-user@ip-172-31-14-247.ap-northeast-1.compute.internal>
2023-03-11 12:19:06 +0000

    keika

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+         has_many :books, dependent: :destroy
+
+         def profile_image
+           unless image.attached?
+             file_path = Rails.root.join('app/assets/images/no_image.jpg')
+             image.attached(io: File.open(file_path),filename: 'default-image.jpg', content_type: 'image/jpeg')
+           end
+           image
+         end
 end
