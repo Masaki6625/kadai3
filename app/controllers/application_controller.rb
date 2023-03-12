565e72febb1e71d53849ba8b71b4e8c571da16d3
EC2 Default User <ec2-user@ip-172-31-14-247.ap-northeast-1.compute.internal>
2023-03-12 12:35:50 +0000

    keika

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -10,7 +11,7 @@ class ApplicationController < ActionController::Base
     flash[:notice]
     root_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
