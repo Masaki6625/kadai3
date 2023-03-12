8e30e823b5221f385c63a6d37e8776e1edc657c0
EC2 Default User <ec2-user@ip-172-31-14-247.ap-northeast-1.compute.internal>
2023-03-12 12:24:20 +0000

    keika

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,21 @@ class UsersController < ApplicationController
     @users = User.all
     @book = Book.new
     @user = current_user
-
   end
 
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
 
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(current_user.id)
@@ -26,5 +28,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 end
